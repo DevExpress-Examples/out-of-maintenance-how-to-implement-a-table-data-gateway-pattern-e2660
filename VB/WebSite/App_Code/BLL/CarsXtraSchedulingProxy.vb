a4367f8421ecb6e19a295b062311c6ecbbc743a6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/App_Code/BLL/CarsXtraSchedulingProxy.vb
@@ -0,0 +1,69 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data.SqlClient
+Imports System.ComponentModel
+Imports CarsXtraSchedulingTableAdapters
+
+<DataObjectAttribute()> _
+Public NotInheritable Class CarsXtraSchedulingProxy
+	Private Shared carSchedulingTableAdapter As CarSchedulingTableAdapter
+	Private Shared carTableAdapter As CarsTableAdapter
+	Private Shared lastInsertedAppointmentId_Renamed As Integer
+
+	Private Sub New()
+	End Sub
+	Public Shared ReadOnly Property LastInsertedAppointmentId() As Integer
+		Get
+			Return lastInsertedAppointmentId_Renamed
+		End Get
+	End Property
+
+	Shared Sub New()
+		carSchedulingTableAdapter = New CarSchedulingTableAdapter()
+		carTableAdapter = New CarsTableAdapter()
+	End Sub
+
+	<DataObjectMethodAttribute(DataObjectMethodType.Select)> _
+	Public Shared Function GetAppointments() As CarsXtraScheduling.CarSchedulingDataTable
+		Return carSchedulingTableAdapter.GetData()
+	End Function
+
+	<DataObjectMethodAttribute(DataObjectMethodType.Select)> _
+	Public Shared Function GetResources() As CarsXtraScheduling.CarsDataTable
+		Return carTableAdapter.GetData()
+	End Function
+
+	<DataObjectMethodAttribute(DataObjectMethodType.Insert)> _
+	Public Shared Function InsertAppointment(ByVal CarId As String, ByVal Status As Nullable(Of Integer), ByVal Subject As String, ByVal Description As String, ByVal Label As Nullable(Of Integer), ByVal StartTime As Nullable(Of DateTime), ByVal EndTime As Nullable(Of DateTime), ByVal Location As String, ByVal AllDay As Boolean, ByVal EventType As Nullable(Of Integer), ByVal RecurrenceInfo As String, ByVal ReminderInfo As String, ByVal Price As Nullable(Of Decimal)) As Boolean
+
+		Dim rowsAffected As Integer = carSchedulingTableAdapter.Insert(CarId, Status, Subject, Description, Label, StartTime, EndTime, Location, AllDay, EventType, RecurrenceInfo, ReminderInfo, Price)
+
+		UpdateLastInsertedAppointmentId()
+
+		Return rowsAffected = 1
+	End Function
+
+	<DataObjectMethodAttribute(DataObjectMethodType.Update)> _
+	Public Shared Function UpdateAppointment(ByVal CarId As String, ByVal Status As Nullable(Of Integer), ByVal Subject As String, ByVal Description As String, ByVal Label As Nullable(Of Integer), ByVal StartTime As Nullable(Of DateTime), ByVal EndTime As Nullable(Of DateTime), ByVal Location As String, ByVal AllDay As Boolean, ByVal EventType As Nullable(Of Integer), ByVal RecurrenceInfo As String, ByVal ReminderInfo As String, ByVal Price As Nullable(Of Decimal), ByVal ID As Integer) As Boolean
+
+		Dim rowsAffected As Integer = carSchedulingTableAdapter.Update(CarId, Status, Subject, Description, Label, StartTime, EndTime, Location, AllDay, EventType, RecurrenceInfo, ReminderInfo, Price, ID)
+
+		Return rowsAffected = 1
+	End Function
+
+	<DataObjectMethodAttribute(DataObjectMethodType.Delete)> _
+	Public Shared Function DeleteAppointment(ByVal ID As Integer) As Boolean
+		Dim rowsAffected As Integer = carSchedulingTableAdapter.Delete(ID)
+
+		Return rowsAffected = 1
+	End Function
+
+	Private Shared Sub UpdateLastInsertedAppointmentId()
+		carSchedulingTableAdapter.Connection.Open()
+		Using command As New SqlCommand("SELECT IDENT_CURRENT('CarScheduling')", carSchedulingTableAdapter.Connection)
+			lastInsertedAppointmentId_Renamed = Convert.ToInt32(command.ExecuteScalar())
+		End Using
+		carSchedulingTableAdapter.Connection.Close()
+	End Sub
+
+End Class
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,29 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Web.ASPxScheduler
+Imports DevExpress.XtraScheduler
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+		If (Not IsPostBack) Then
+			ASPxScheduler1.Start = New DateTime(2008, 7, 12)
+		End If
+	End Sub
+
+	Protected Sub ASPxScheduler1_AppointmentRowInserting(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxScheduler.ASPxSchedulerDataInsertingEventArgs)
+		e.NewValues.Remove("ID")
+	End Sub
+
+	Protected Sub ASPxScheduler1_AppointmentRowInserted(ByVal sender As Object, ByVal e As DevExpress.Web.ASPxScheduler.ASPxSchedulerDataInsertedEventArgs)
+		e.KeyFieldValue = CarsXtraSchedulingProxy.LastInsertedAppointmentId
+	End Sub
+
+	Protected Sub ASPxScheduler1_AppointmentsInserted(ByVal sender As Object, ByVal e As DevExpress.XtraScheduler.PersistentObjectsEventArgs)
+		Dim apt As Appointment = CType(e.Objects(0), Appointment)
+		Dim storage As ASPxSchedulerStorage = CType(sender, ASPxSchedulerStorage)
+		storage.SetAppointmentId(apt, CarsXtraSchedulingProxy.LastInsertedAppointmentId)
+	End Sub
+
+End Class
